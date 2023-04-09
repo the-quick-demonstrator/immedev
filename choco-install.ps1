1909bc1d8e60d94d530dc58dbaf7ab11387a4e17
git-leon <xleonhunter@gmail.com>
2023-04-09 14:48:09 -0400

    update

--- choco-install.ps1
+++ choco-install.ps1
@@ -3,13 +3,17 @@ function chocoInstall {
         [string]$packageName
     )
 
-    $installedPackages = choco list -local-only | Select-String -Pattern $packageName
+    try {
+        $installedPackages = choco list -local-only | Select-String -Pattern $packageName
 
-    if ($installedPackages) {
-        Write-Host "$packageName is already installed."
-    } else {
-        Write-Host "$packageName is being installed..."
-        choco install "$packageName" -f -y
-        Write-Host "$packageName has been installed!"
+        if ($installedPackages) {
+            Write-Host "$packageName is already installed."
+        } else {
+            Write-Host "$packageName is being installed..."
+            choco install "$packageName" -f -y
+            Write-Host "$packageName has been installed!"
+        }
+    } catch {
+            Write-Host "An error occurred while installing $packageName"
     }
 }
