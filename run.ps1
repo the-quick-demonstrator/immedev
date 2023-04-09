162c34299bb1433baa4e24aff6bab2b821503078
git-leon <xleonhunter@gmail.com>
2023-04-09 14:44:16 -0400

    update

---
+++ run.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+# Get a list of all .ps1 files in the current directory
+$installScripts = Get-ChildItem -Path . -Filter install*.ps1 -File
+
+# Iterate through the list and execute each script
+foreach ($script in $installScripts) {
+    Write-Host "Running $($script.FullName)..."
+    & $script.FullName
+}
