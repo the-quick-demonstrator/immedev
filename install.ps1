dace769d26194686334ba7ee123a1a12feae2530
git-leon <xleonhunter@gmail.com>
2023-04-20 17:37:16 -0400

    bypassed execution policy

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
 # Get a list of all .ps1 files in the current directory
-$installScripts = Get-ChildItem -Path . -Filter install*.ps1 -File
+$installScripts = Get-ChildItem -Path . -Filter install-*.ps1 -File
 
 # Iterate through the list and execute each script
 foreach ($script in $installScripts) {
